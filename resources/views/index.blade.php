ccc77809911b4ad884e316034c3289105137662f
Peter Steele <petersteele111@gmail.com>
2023-12-19 05:25:23 -0500

    Created Home Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,22 @@
             @endif
         @endauth
     </div>
-@endif
-    <div class="bg-cover bg-center h-screen flex items-center justify-center" style="background-image: url({{ asset('img/home.jpg') }});">
-        <h1 class="text-6xl text-white font-bold">Welcome to My Photography Website</h1>
+    @endif
+    <div class="relative h-screen bg-cover bg-center" style="background-image: url({{ asset('img/home.jpg') }});">
+        <!-- Overlay with opacity -->
+        <div class="absolute inset-0 bg-black bg-opacity-50"></div>
+    
+        <!-- Content -->
+        <div class="relative flex flex-col items-center justify-center h-full pt-20">
+            <img src="{{ asset('img/logo.png') }}" alt="EUP Photography Logo" class="max-h-96 mb-4">
+            <h1 class="text-7xl text-white font-bold p-4 text-center mx-4">Capturing Timeless Moments</h1>
+            <p class="text-3xl text-white font-semibold mt-4 p-4 text-center mx-4">Specializing in Landscape and Portrait Photography</p>
+
+        <!-- Button with a class for the hover effect -->
+        <a href="{{ route('Contact') }}" class="cta-button mt-8 px-6 py-3 bg-white text-gray-800 font-semibold rounded-md shadow hover:bg-gray-100 transition ease-in-out duration-150">
+            Book Your Session Now
+        </a>
+
+        </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,50 +1,50 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-20"> <!-- Increased height to h-20 -->
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('Home') }}">
+                        <img src="{{ asset('img/logo.png') }}" alt="Logo" height="50" width="100" class="m-4">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 @auth
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-lg"> <!-- Increased font-size with text-lg -->
                             {{ __('Dashboard') }}
                         </x-nav-link>
                     </div>
                 @endauth
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Home')" :active="request()->routeIs('Home')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('Home')" :active="request()->routeIs('Home')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('Home') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Services')" :active="request()->routeIs('Services')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('Services')" :active="request()->routeIs('Services')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('Services') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Pricing')" :active="request()->routeIs('Pricing')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('Pricing')" :active="request()->routeIs('Pricing')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('Pricing') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Portfolio')" :active="request()->routeIs('Portfolio')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('Portfolio')" :active="request()->routeIs('Portfolio')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('Portfolio') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('About')" :active="request()->routeIs('About')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('About')" :active="request()->routeIs('About')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('About') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Contact')" :active="request()->routeIs('Contact')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
+                    <x-nav-link :href="route('Contact')" :active="request()->routeIs('Contact')" class="text-lg"> <!-- Increased font-size with text-lg -->
                         {{ __('Contact') }}
                     </x-nav-link>
                 </div>
@@ -52,7 +52,7 @@
 
             <!-- Settings Dropdown -->
             @auth
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="hidden lg:flex sm:items-center sm:ms-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -87,7 +87,7 @@
             @endauth
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center lg:hidden z-50">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -99,7 +99,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
         @auth
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
