b42e10050cb61e335241b2efc300de9c0905dc2e
Peter Steele <petersteele111@gmail.com>
2023-12-21 00:47:59 -0500

    Styled the Homepage more to my liking

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,21 +10,17 @@
         @endauth
     </div>
     @endif
-    <div class="relative h-screen bg-cover bg-center" style="background-image: url({{ asset('img/home.jpg') }});">
-        <!-- Overlay with opacity -->
-        <div class="absolute inset-0 bg-black bg-opacity-50"></div>
-    
-        <!-- Content -->
-        <div class="relative flex flex-col items-center justify-center h-full pt-20">
-            <img src="{{ asset('img/logo.png') }}" alt="EUP Photography Logo" class="max-h-96 mb-4">
-            <h1 class="text-7xl text-white font-bold p-4 text-center mx-4">Capturing Timeless Moments</h1>
-            <p class="text-3xl text-white font-semibold mt-4 p-4 text-center mx-4">Specializing in Landscape and Portrait Photography</p>
-
-        <!-- Button with a class for the hover effect -->
-        <a href="{{ route('Contact') }}" class="cta-button mt-8 px-6 py-3 bg-white text-gray-800 font-semibold rounded-md shadow hover:bg-gray-100 transition ease-in-out duration-150">
-            Book Your Session Now
-        </a>
-
+    <div class="relative bg-cover bg-center text-white" style="background-image: url({{ asset('img/home.jpg') }}); height: 100vh;">
+        <div class="absolute inset-0 bg-black bg-opacity-20"></div>
+        <div class="relative w-full flex flex-col h-screen items-center justify-center">
+            <div class="mt-4 content-center">
+                <p class=" text-5xl sm:text-7xl xl:text-9xl">Landscape & Portrait </br> Photography</p>
+            </div>
+            <div class="mt-48 text-2xl xl:text-4xl">
+                <a href="#contact" class="bg-red-600 hover:bg-red-800 text-white font-bold py-4 px-8 rounded-full">
+                    Work With Me
+                </a>
+            </div>
         </div>
     </div>
 </x-app-layout>
