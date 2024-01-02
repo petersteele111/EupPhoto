b73ea4b267b57e65e42f278a599c2b2e716e017a
Peter Steele <petersteele111@gmail.com>
2024-01-01 23:12:00 -0500

    Added Directory Colum to the Photos Table so that they can be properly recalled no matter where they are stored.

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -1,13 +1,24 @@
 <x-app-layout>
-    @foreach ($photos as $photo)
-    <div>
-        <img src="{{ $photo->url }}" alt="{{ $photo->name }}">
-        <a href="{{ route('photos.edit', $photo->id) }}">Edit</a>
-        <form method="POST" action="{{ route('photos.destroy', $photo->id) }}">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>
+    <div class="container mx-auto px-4">
+        <div class="flex flex-wrap -mx-4">
+            @foreach ($photos as $photo)
+                <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 mb-8">
+                    <div class="bg-white shadow rounded-lg overflow-hidden">
+                        <img src="{{ asset('images/' . $photo->fileName) }}" alt="{{ $photo->title }}" class="w-full h-64 object-cover">
+                        <div class="p-6">
+                            <h2 class="text-2xl font-bold">{{ $photo->title }}</h2>
+                            <div class="mt-4">
+                                <a href="{{ route('photos.edit', $photo->id) }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Edit</a>
+                                <form method="POST" action="{{ route('photos.destroy', $photo->id) }}" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" onclick="return confirm('Are you sure you want to delete this photo?')" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-@endforeach
 </x-app-layout>
