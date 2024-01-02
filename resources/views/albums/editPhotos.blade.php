4229c3151ae7ed4e0a0e844e97aca8fdd0cbb91e
Peter Steele <petersteele111@gmail.com>
2024-01-02 18:42:56 -0500

    Made the image title smaller when viewing them for edit or delete

--- resources/views/albums/editPhotos.blade.php
+++ resources/views/albums/editPhotos.blade.php
@@ -9,7 +9,7 @@
                     <div class="bg-white shadow rounded-lg overflow-hidden">
                         <img src="{{ asset('storage/' . $photo->directory . '/' . $photo->fileName) }}" alt="{{ $photo->title }}" class="w-full h-64 object-cover">
                         <div class="p-6">
-                            <h2 class="text-2xl font-bold">{{ $photo->title }}</h2>
+                            <h2 class="text-xl font-bold">{{ $photo->title }}</h2>
                             <div class="mt-4">
                                 <a href="{{ route('photos.edit', $photo->id) }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Edit</a>
                                 <form method="POST" action="{{ route('photos.destroy', $photo->id) }}" class="inline-block">
