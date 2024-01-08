bbea527dded80a4cd3eeaf4a507e44a505365e4f
Peter Steele <petersteele111@gmail.com>
2024-01-08 17:27:33 -0500

    fixed a weird issue where the show all is not the right size, because the other buttons are somehow getting mt-4 added to them

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -3,11 +3,11 @@
         <div class="popular-photos" id="next-section">
             <h2 class="uppercase tracking-wider text-white text-5xl text-center font-semibold">Portfolio</h2>
             <p class="text-white text-center text-xl my-12">Here are some of my favorite shots, I hope you enjoy!</p>
-            <div class="flex flex-col sm:flex-row flex-wrap justify-center space-y-4 sm:space-y-0 sm:space-x-4 mb-8">
-                <button class="text-white text-3xl sm:text-md xs:text-sm bg-gray-800 px-4 py-2 rounded hover:bg-gray-700 mb-2 sm:mb-0" data-filter="*">Show All</button>
+            <div class="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 mb-8">
+                <button class="text-white text-3xl bg-gray-800 px-4 py-2 rounded hover:bg-gray-700 mb-2 sm:mb-0 mx-2 mt-4" data-filter="*">Show All</button>
                 @foreach($categories as $category)
                     @if(!empty($category->category))
-                        <button class="text-white text-3xl sm:text-md xs:text-sm bg-gray-800 px-4 py-2 rounded hover:bg-gray-700 mb-2 sm:mb-2" data-filter=".{{ $category->category }}">{{ ucfirst($category->category) }}</button>
+                        <button class="text-white text-3xl bg-gray-800 px-4 py-2 rounded hover:bg-gray-700 mb-2 sm:mb-0 mx-2" data-filter=".{{ $category->category }}">{{ ucfirst($category->category) }}</button>
                     @endif
                 @endforeach
             </div>
