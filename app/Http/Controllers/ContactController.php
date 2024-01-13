2721c2d80093c8d7a0d8db5cd57a18391e9a8a73
Peter Steele <petersteele111@gmail.com>
2024-01-13 01:41:14 -0500

    Fixed the success message so that it actually appears on the contact page after submission

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -25,6 +25,6 @@ class ContactController extends Controller
 
         Mail::to(env('CONTACT_FORM_EMAIL'))->send(new ContactFormMailable($data));
 
-        return redirect('contact')->with('message', 'Thanks for your message. We\'ll be in touch soon. If this is urgent, please call at 906-349-1202. Thank you for your interest and we look forward to working with you!');
+        return redirect('contact')->with('success', 'Thanks for your message. We\'ll be in touch soon. If this is urgent, please call at 906-349-1202. Thank you for your interest and we look forward to working with you!');
     }
 }
